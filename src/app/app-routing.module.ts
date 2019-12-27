183f4f7d5f95df8e30933ba14366e443c76b709b
Renato Anunciação <wagnerpunk002@gmail.com>
2019-12-27 09:52:45 -0300

    inserção do button de contagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { DetailComponent } from "./pages/detail/detail.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: "",
+  component: HomeComponent,
+  pathMatch: "full"
+  },
+  {
+  path: "detail/:albumId",
+  component: DetailComponent
+  },
+  {
+  path: "**",
+  component: HomeComponent
+  }
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,57 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HeaderComponent } from './features/header/header.component';
+import { SearchComponent } from './features/search/search.component';
+import { AlbumListComponent } from './features/album-list/album-list.component';
+import { AlbumBoxComponent } from './features/album-box/album-box.component';
+import { UserInfoComponent } from './features/user-info/user-info.component';
+import { PhotoListComponent } from './features/photo-list/photo-list.component';
+import { PhotoViewComponent } from './features/photo-view/photo-view.component';
+import { HttpClientModule } from "@angular/common/http";
+
+// Angular Material
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    HeaderComponent,
+    SearchComponent,
+    AlbumListComponent,
+    AlbumBoxComponent,
+    UserInfoComponent,
+    PhotoListComponent,
+    PhotoViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatCardModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [PhotoViewComponent]
 })
 export class AppModule { }
